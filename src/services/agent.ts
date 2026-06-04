ca99855a2d37b589557138540eabb0f2c0c43ae5
Tanuj Pant <tanujpant13@gmail.com>
2026-06-04 14:08:15 +0530

    implement separation of concerns via a tool registry, an agent and a harness class

---
+++ src/services/agent.ts
@@ -0,0 +1,53 @@
+import {
+  FunctionCallingConfigMode,
+  GoogleGenAI,
+  type Content,
+  type Part,
+} from "@google/genai";
+import type { ToolRegistry } from "./tools";
+
+export class Agent {
+  private ai: GoogleGenAI;
+  private history: Content[] = [];
+  private model = "gemma-4-26b-a4b-it";
+
+  constructor(apiKey: string, initialPrompt: string) {
+    this.ai = new GoogleGenAI({ apiKey });
+    this.addUserRole([{ text: initialPrompt }]);
+  }
+
+  getHistory() {
+    return this.history;
+  }
+
+  addUserRole(parts: Part[]) {
+    this.history.push({
+      role: "user",
+      parts,
+    });
+  }
+
+  addModelRole(parts: Part[]) {
+    this.history.push({
+      role: "model",
+      parts,
+    });
+  }
+
+  async runStep(registry: ToolRegistry) {
+    const declarations = registry.getGiminiDeclarations();
+    return await this.ai.models.generateContent({
+      model: this.model,
+      contents: this.history,
+      config: {
+        toolConfig: {
+          functionCallingConfig: {
+            mode: FunctionCallingConfigMode.AUTO,
+            // allowedFunctionNames: ["readFile", "writeFile"],
+          },
+        },
+        tools: [{ functionDeclarations: declarations }],
+      },
+    });
+  }
+}
---
+++ src/services/harness.ts
@@ -0,0 +1,76 @@
+import type { Agent } from "./agent";
+import type { ToolRegistry } from "./tools";
+
+export class Harness {
+  private agent: Agent;
+  private toolRegistry: ToolRegistry;
+  private maxIterations;
+
+  status = "pending";
+
+  constructor(agent: Agent, toolRegistry: ToolRegistry, maxIterations = 5) {
+    this.agent = agent;
+    this.toolRegistry = toolRegistry;
+    this.maxIterations = maxIterations;
+  }
+
+  async executeTask() {
+    let iteration = 0;
+    let processing = true;
+    while (processing && iteration < this.maxIterations) {
+      iteration++;
+      const response = await this.agent.runStep(this.toolRegistry);
+
+      if (response.functionCalls) {
+        this.agent.addModelRole(
+          response.functionCalls.map((call) => ({ functionCall: call })),
+        );
+
+        const toolResponseParts = [];
+        for (const fn of response.functionCalls) {
+          const tool = this.toolRegistry.get(fn.name!);
+          if (!tool) {
+            console.warn(
+              `LLM attempted to call unregistered tool: "${fn.name}"`,
+            );
+            continue;
+          }
+
+          const parseResult = tool.schema.safeParse(fn.args);
+          if (!parseResult.success) {
+            console.error(
+              `Validation failed for tool '${tool.name}'. Errors:`,
+              parseResult.error.flatten(),
+            );
+            continue;
+          }
+
+          try {
+            const result = await tool.execute(parseResult.data as any);
+            toolResponseParts.push({
+              functionResponse: {
+                name: tool.name,
+                response: result,
+              },
+            });
+          } catch (err) {
+            console.error(`Runtime error executing ${tool.name}`, err);
+          }
+        }
+
+        console.log("toolResponseParts", toolResponseParts);
+
+        this.agent.addUserRole(toolResponseParts);
+      } else {
+        // no tool calls
+        console.log("Final Response: ", response.text);
+        console.dir(response.usageMetadata, { depth: 5 });
+        processing = false;
+      }
+    }
+
+    if (iteration >= this.maxIterations) {
+      console.warn("Harness hit safety iteration limit guardrail.");
+    }
+  }
+}
